495fdf49d1ffbc7cdf67179ee54159f50737750f
Benjamin Bittner <benjamin.bittner@omigos.de>
2014-06-23 09:47:22 +0200

    #9 fixed github CSP for images and frames

--- src/lib/background.js
+++ src/lib/background.js
@@ -4,6 +4,8 @@ chrome.webRequest.onHeadersReceived.addListener(
         if (details.responseHeaders[i].name.toUpperCase() == 'CONTENT-SECURITY-POLICY') {
           var csp = details.responseHeaders[i].value;
           csp = csp.replace('script-src', 'script-src https://*.harvestapp.com');
+          csp = csp.replace('img-src', 'img-src https://*.harvestapp.com');
+          csp = csp.replace('frame-src', 'frame-src https://*.harvestapp.com');
           csp = csp.replace('style-src', 'style-src https://*.harvestapp.com');
           csp = csp.replace('connect-src', 'connect-src https://*.harvestapp.com');
           details.responseHeaders[i].value = csp;
