a2d83925c0ddc4ab399805d65f75a12a86540073
Benjamin Bittner <benjamin.bittner@omigos.de>
2015-03-16 13:22:50 +0100

    #13 updated app to be allowed to run on every page, but integrated other mechanism to prohibit it from really running on every page

--- src/lib/background.js
+++ src/lib/background.js
@@ -1,27 +1,30 @@
-chrome.webRequest.onHeadersReceived.addListener(
-    function (details) {
-      for (i = 0; i < details.responseHeaders.length; i++) {
-        if (details.responseHeaders[i].name.toUpperCase() == 'CONTENT-SECURITY-POLICY') {
-          var csp = details.responseHeaders[i].value;
-          csp = csp.replace('script-src', 'script-src https://*.harvestapp.com');
-          csp = csp.replace('img-src', 'img-src https://*.harvestapp.com');
-          csp = csp.replace('frame-src', 'frame-src https://*.harvestapp.com');
-          csp = csp.replace('style-src', 'style-src https://*.harvestapp.com');
-          csp = csp.replace('connect-src', 'connect-src https://*.harvestapp.com');
-          details.responseHeaders[i].value = csp;
-        }
-      }
-      return { responseHeaders : details.responseHeaders };
-    },
-    {
-      urls : [
-          "https://github.com/*",
-          "https://huboard.com/*"
-      ],
-      types : ["main_frame"]
-    },
-    [
-      "blocking",
-      "responseHeaders"
+chrome.storage.sync.get({
+    activeUrls: [
+        "https://github.com/",
+        "https://huboard.com/"
     ]
-);
+}, function(items) {
+    var urls = [];
+    for(var i = 0; i < items.activeUrls.length; i++) {
+        urls.push(items.activeUrls[i] + '*');
+    }
+
+    chrome.webRequest.onHeadersReceived.addListener(
+        function (details) {
+            for (i = 0; i < details.responseHeaders.length; i++) {
+                if (details.responseHeaders[i].name.toUpperCase() == 'CONTENT-SECURITY-POLICY') {
+                    var csp = details.responseHeaders[i].value;
+                    csp = csp.replace('script-src', 'script-src https://*.harvestapp.com');
+                    csp = csp.replace('img-src', 'img-src https://*.harvestapp.com');
+                    csp = csp.replace('frame-src', 'frame-src https://*.harvestapp.com');
+                    csp = csp.replace('style-src', 'style-src https://*.harvestapp.com');
+                    csp = csp.replace('connect-src', 'connect-src https://*.harvestapp.com');
+                    details.responseHeaders[i].value = csp;
+                }
+            }
+            return { responseHeaders : details.responseHeaders };
+        },
+        { urls : urls, types : ["main_frame"] },
+        [ "blocking", "responseHeaders" ]
+    );
+});
